f84dc066b2928c5eaa9b5a0e8df7e3f468dcaaf4
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-26 15:05:27 +0330

    add consulter middleware to app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'consulter_middleware' => \App\Http\Middleware\Consulter\ConsulterMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
