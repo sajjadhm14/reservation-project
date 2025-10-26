97f79862cc2c51415758fac12b57ac6bcbb5c1d6
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-26 15:48:19 +0330

    modify consulter dashboard controller

--- app/Http/Controllers/Consulter/Dashboard/ConsulterDashboardController.php
+++ app/Http/Controllers/Consulter/Dashboard/ConsulterDashboardController.php
@@ -11,7 +11,7 @@ class ConsulterDashboardController extends Controller
     public function index()
     {
 
-        return view('consulter.dashboard.dashboard');
+        return view('consulter.dashboard.pages.profile');
     }
 
     public function logout(Request $request)
