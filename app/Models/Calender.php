228937fe728a575b092e2ec999170188c8007d02
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-27 11:52:54 +0330

    modify calender model to add relations

--- app/Models/Calender.php
+++ app/Models/Calender.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Calender extends Model
 {
-    //
+    protected $fillable = [
+        'consulter_id',
+        'date',
+        'start_time',
+        'end_time',
+    ];
+
+    public function consulter() :belongsTo
+    {
+        return $this->belongsTo(Consulter::class);
+    }
 }
