eb46978216a90e2691342a07f698db274a6bf688
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-27 15:50:22 +0330

    modify to make relation between User.php and reservation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -33,6 +34,10 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public static function Auth()
+    {
+    }
+
     /**
      * Get the attributes that should be cast.
      *
@@ -44,5 +49,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function reservations():hasMany
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
