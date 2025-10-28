814fb20158097b6c2fde31321b516dca59ba9b6c
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-28 16:11:48 +0330

    modify Calender.php to add amount to model

--- app/Models/Calender.php
+++ app/Models/Calender.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Calender extends Model
 {
     protected $fillable = [
         'consulter_id',
+        'status',
         'date',
+        'amount',
         'start_time',
         'end_time',
     ];
@@ -18,4 +22,10 @@ class Calender extends Model
     {
         return $this->belongsTo(Consulter::class);
     }
+
+    public function reservation():HasOne
+    {
+        return $this->hasOne(Reservation::class);
+    }
+
 }
