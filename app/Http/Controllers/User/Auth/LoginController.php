1d91d9da9e2a519870eb31bba1cb579de7da7fff
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-28 16:13:10 +0330

    modify user login controller

--- app/Http/Controllers/User/Auth/LoginController.php
+++ app/Http/Controllers/User/Auth/LoginController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\User\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\Auth\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-    public function index(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+    public function index()
     {
+        if(Auth::guard('web')->check()){
+            return redirect()->route('user.dashboard');
+        }
         return view('user.auth.login');
     }
 
