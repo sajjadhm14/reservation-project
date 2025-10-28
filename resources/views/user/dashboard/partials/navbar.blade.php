b21d283127a3831ce427f4baab8483e268d5a481
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-28 16:13:32 +0330

    modify user navbar to add wallet

--- resources/views/user/dashboard/partials/navbar.blade.php
+++ resources/views/user/dashboard/partials/navbar.blade.php
@@ -12,11 +12,10 @@
         <ul class="navbar-nav">
 
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="messageDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('wallet')}}" >
                     <i data-feather="mail"></i>
                 </a>
-
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="notificationDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
