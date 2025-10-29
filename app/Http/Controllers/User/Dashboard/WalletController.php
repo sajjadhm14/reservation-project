f4f38fc7e42bf43143efdf17c09c1c788b60cc62
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-29 09:06:55 +0330

    make wallet post methode in wallet controller

--- app/Http/Controllers/User/Dashboard/WalletController.php
+++ app/Http/Controllers/User/Dashboard/WalletController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\User\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\Dashboard\WalletRequest;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 
 class WalletController extends Controller
@@ -11,4 +13,19 @@ class WalletController extends Controller
     {
         return view('user.dashboard.pages.wallet');
     }
+    public function walletCharge()
+    {
+        return view('user.dashboard.pages.wallet-charge');
+    }
+    public function walletPost(WalletRequest $request)
+    {
+        $request->validated();
+
+        $wallet = Wallet::firstOrCreate(['user_id' => auth()->id()]);
+        $wallet->current_balance += $request->amount;
+        $wallet->save();
+
+        return redirect('user/dashboard')->with('success', "Payment successful! \${$request->amount} has been added to your wallet.");
+    }
+
 }
