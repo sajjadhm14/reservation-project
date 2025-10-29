1ba214eb09e1a4ba61d709cac02289375647dc70
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-29 17:39:03 +0330

    make gateway controller

---
+++ app/Http/Controllers/User/Transactions/GatewayController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\User\Transactions;
+
+use App\Http\Controllers\Controller;
+
+class GatewayController extends Controller
+{
+    public function startPayment()
+    {
+
+    }
+}
