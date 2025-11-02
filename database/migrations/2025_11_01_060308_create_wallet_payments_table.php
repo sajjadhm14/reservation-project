39afae935b22c37382ee966086ac91d3691199a2
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-02 14:01:49 +0330

    create payment and wallet payment table

---
+++ database/migrations/2025_11_01_060308_create_wallet_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallet_payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('wallet_id')->constrained('wallets')->onDelete('cascade');
+            $table->string('amount');
+            $table->bigInteger('ref_id');
+            $table->string('ref_number');
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallet_payments');
+    }
+};
