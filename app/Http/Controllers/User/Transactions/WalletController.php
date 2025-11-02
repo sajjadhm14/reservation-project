094edf7d912c234aae4f753754dbe4fe394ffd18
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-02 13:55:33 +0330

    modify wallet controller

--- app/Http/Controllers/User/Transactions/WalletController.php
+++ app/Http/Controllers/User/Transactions/WalletController.php
@@ -18,26 +18,7 @@ class WalletController extends Controller
     {
         return view('user.dashboard.transactions.wallet-charge');
     }
-    public function walletPost(WalletRequest $request): \Illuminate\Http\RedirectResponse
-    {
-        $user = auth()->user();
 
-        $wallet = Wallet::firstOrCreate(['user_id' => $user->id]);
-
-        $wallet->current_balance += $request->amount;
-        $wallet->save();
-
-        $totalPaid = $user->reservations()->sum('amount');
-        $remainingBalance = $wallet->current_balance - $totalPaid;
-
-         $wallet->remaining_balance = $remainingBalance;
-         $wallet->save();
-
-        return redirect('user/dashboard')->with([
-            'success' => "Transaction successful! \${$request->amount} has been added to your wallet.",
-            'remaining_balance' => $remainingBalance,
-        ]);
-    }
 
 
 }
