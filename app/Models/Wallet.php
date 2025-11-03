15b908b5e1d1cd4d62da2589cebeecd85147c8ed
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-03 16:53:09 +0330

    modify Wallet.php to add relations

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Wallet extends Model
 {
@@ -11,4 +13,13 @@ class Wallet extends Model
         'current_balance',
         'remaining_balance',
     ];
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function walletPayments():HasMany
+    {
+        return $this->hasMany(WalletPayment::class);
+    }
 }
