1d1c4f8a562773cb82374cff6c61fdf5a621e2b3
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-08 10:12:36 +0330

    apply minor adjustments to navbar.blade.php

--- resources/views/user/dashboard/partials/navbar.blade.php
+++ resources/views/user/dashboard/partials/navbar.blade.php
@@ -17,23 +17,14 @@
                     <i data-feather="mail"></i>
                 </a>
             </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="notificationDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i data-feather="bell"></i>
-                    <div class="indicator">
-                        <div class="circle"></div>
-                    </div>
-                </a>
-            </li>
+
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <img class="wd-30 ht-30 rounded-circle" src="{{ asset('user/uploads/no-img-avatar.png')}}" alt="profile">
                 </a>
                 <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
                     <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
-                        <div class="mb-3">
-                            <img class="wd-80 ht-80 rounded-circle" src="{{ asset('user/uploads/no-img-avatar.png')}}"alt="">
-                        </div>
+
                         <div class="text-center">
                             <p class="tx-16 fw-bolder">{{$user->name}}</p>
                             <p class="tx-12 text-muted">{{$user->email}}</p>
