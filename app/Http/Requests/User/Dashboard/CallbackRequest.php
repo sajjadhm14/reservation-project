cc40f3b0aaa9f6ff05beb12847cf21a5be1f24ed
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-09 09:25:09 +0330

    make callback request for GatewayController.php

---
+++ app/Http/Requests/User/Dashboard/CallbackRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\User\Dashboard;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CallbackRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status' => 'required|in:success,failed,cancelled',
+            'ref_id' => 'required|integer|exists:wallet_payments,ref_id',
+        ];
+    }
+}
