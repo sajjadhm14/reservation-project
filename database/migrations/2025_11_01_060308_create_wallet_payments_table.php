b3a164b82bab878208ba9e2031772295beada457
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-09 09:25:43 +0330

    modify wallet payments table

--- database/migrations/2025_11_01_060308_create_wallet_payments_table.php
+++ database/migrations/2025_11_01_060308_create_wallet_payments_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('wallet_id')->constrained('wallets')->onDelete('cascade');
-            $table->string('amount');
-            $table->string('ref_id');
+            $table->integer('amount');
+            $table->integer('ref_id');
             $table->string('ref_number')->nullable();
             $table->string('status')->nullable();
             $table->timestamps();
