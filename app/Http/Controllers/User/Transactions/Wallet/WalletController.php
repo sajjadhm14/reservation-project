83841d9a638da1899a2e8dc459eb40bfc8f1d007
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-10 09:58:59 +0330

    modify WalletController.php

--- app/Http/Controllers/User/Transactions/Wallet/WalletController.php
+++ app/Http/Controllers/User/Transactions/Wallet/WalletController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers\User\Transactions\Wallet;
 
 use App\Http\Controllers\Controller;
+use App\Models\ReservationPayment;
 use App\Models\Wallet;
+use App\Models\WalletPayment;
 use Illuminate\Support\Facades\Auth;
 
 class WalletController extends Controller
 {
     public function index()
     {
-        $historys = Wallet::where('user_id' , Auth::guard('web')->id())->get();
-        return view('user.dashboard.transactions.wallet' , compact('historys'));
+
+        $historys = WalletPayment::where('user_id' , Auth::guard('web')->id())
+        ->orderby('created_at' , 'desc')
+        ->take(5)
+        ->get();
+        return view('user.dashboard.transactions.wallet' , compact('historys' ));
     }
     public function walletCharge()
     {
