b7838aa3c13adad382394b1582579fe7df604fbe
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-11 17:36:43 +0330

    add doc block and static methods to Reservation.php

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Reservation extends Model
 {
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'consulter_id',
         'consulter_name',
@@ -21,19 +24,64 @@ class Reservation extends Model
         'end_time',
     ];
 
+    /**
+     * @param $user
+     * @return mixed
+     */
+    public static function getUserReservationStatus($user)
+    {
+        return Reservation::where('user_id' ,$user)
+            ->where('status' , 'Approved')
+            ->get();
+    }
+
+    /**
+     * @param $user
+     * @return mixed
+     */
+    public static function getUserPaymentStatus($user)
+    {
+        return Reservation::where('user_id' ,$user->id)
+            ->where('status' , 'Approved')
+            ->first();
+    }
+
+    /**
+     * @param $user
+     * @return mixed
+     */
+    public static function getUserReservations($user)
+    {
+        return Reservation::where('user_id',$user)->get();
+    }
+
+    /**
+     * @return BelongsTo
+     */
     public function user():belongsTo
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * @return BelongsTo
+     */
     public function consulter():belongsTo
     {
         return $this->belongsTo(Consulter::class);
     }
+
+    /**
+     * @return BelongsTo
+     */
     public function calender():belongsTo
     {
         return $this->belongsTo(Calender::class);
     }
+
+    /**
+     * @return HasOne
+     */
     public function reservationPayment():HasOne
     {
         return $this->hasOne(ReservationPayment::class);
