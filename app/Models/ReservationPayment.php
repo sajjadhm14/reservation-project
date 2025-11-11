cbf503c23c6bef9b704db2d9b8005d83c5e2711e
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-11 17:37:27 +0330

    modify ReservationPayment.php to add doc block

--- app/Models/ReservationPayment.php
+++ app/Models/ReservationPayment.php
@@ -7,15 +7,26 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReservationPayment extends Model
 {
+    /**
+     * @var string[]
+     */
     protected $fillable =  [
         'reservation_id',
         'user_id',
         'status',
     ];
+
+    /**
+     * @return BelongsTo
+     */
     public function reservation():BelongsTo
     {
         return $this->belongsTo(Reservation::class);
     }
+
+    /**
+     * @return BelongsTo
+     */
     public function user():BelongsTo
     {
         return $this->belongsTo(User::class);
