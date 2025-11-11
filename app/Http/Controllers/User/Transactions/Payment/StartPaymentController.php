32f45d5b3bd2187a16c9208142e39afc313a30a0
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-11 11:21:50 +0330

    update the version of StartPaymentController.php.php

--- app/Http/Controllers/User/Transactions/Payment/StartPaymentController.php
+++ app/Http/Controllers/User/Transactions/Payment/StartPaymentController.php
@@ -3,28 +3,48 @@
 namespace App\Http\Controllers\User\Transactions\Payment;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\User\Transactions\PaymentRequest;
+use App\Http\Requests\User\Transactions\UserPaymentRequest;
 use App\Models\Wallet;
 use App\Models\WalletPayment;
+use Illuminate\Support\Facades\Auth;
 
 class StartPaymentController extends Controller
 {
 
 
 
-    public function paymentPost(PaymentRequest $request)
+    public function paymentPost(UserPaymentRequest $request)
     {
 
         $valid = $request->validated();
-        $user = Auth()->user();
-        Wallet::firstOrCreate(['user_id'=>$user->id]);
+        $user = Auth::guard('web')->id();
+        $wallet=Wallet::firstOrCreate(['user_id'=>$user]);
         $amount = $valid['amount'];
         $ref_id = random_int(100000, 999999);
+        $this->createPaymentPost($ref_id ,$amount,$wallet ,$user);
 
-
-        return view('user.dashboard.transactions.payment', ['ref_id' => $ref_id , 'amount' => $amount]);
+        return redirect()->route('payment.page' , ['ref_id'=>$ref_id,'amount'=>$amount  ]);
     }
 
+    private function createPaymentPost($ref_id ,$amount,$wallet ,$user)
+    {
+        return  WalletPayment::create([
+            'wallet_id' => $wallet->id,
+            'amount' => $amount,
+            'ref_id' => $ref_id,
+            'user_id' =>$user,
+            'status' => 'pending',
+            'ref_number' => null,
+        ]);
+    }
+
+    public function paymentPage(UserPaymentRequest $request)
+    {
+        $amount = $request->amount;
+        $ref_id = $request->ref_id;
+        return view('user.dashboard.wallet.transactions.payment' , compact('amount','ref_id' ,));
+
+    }
 
 
 
