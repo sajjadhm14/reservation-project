c93e935e67e3c7dca178c071c14b103eac9f43d9
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-11 17:34:43 +0330

    modify ConsulterDashboardController.php to add docblock

--- app/Http/Controllers/Consulter/Dashboard/ConsulterDashboardController.php
+++ app/Http/Controllers/Consulter/Dashboard/ConsulterDashboardController.php
@@ -8,12 +8,19 @@ use Illuminate\Support\Facades\Auth;
 
 class ConsulterDashboardController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index()
     {
 
         return view('consulter.dashboard.pages.profile');
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function logout(Request $request)
     {
         Auth::guard('web')->logout();
