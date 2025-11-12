3d4875e0f5c035b27e786c42836f6955764c30f4
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-12 09:45:28 +0330

    modify WalletPayment.php

--- app/Models/WalletPayment.php
+++ app/Models/WalletPayment.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class WalletPayment extends Model
 {
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'wallet_id',
         'amount',
@@ -16,10 +20,37 @@ class WalletPayment extends Model
         'ref_number',
     ];
 
+    /**
+     * @return mixed
+     */
+    public static function getUserWallet()
+    {
+        return WalletPayment::where('user_id' , Auth::guard('web')->id())
+            ->orderby('created_at' , 'desc')
+            ->take(5)
+            ->get();
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public static function getPaymentRefId($data)
+    {
+        return WalletPayment::where('ref_id' ,$data)->firstOrFail();
+    }
+
+    /**
+     * @return BelongsTo
+     */
     public function user():BelongsTo
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * @return BelongsTo
+     */
 
     public function wallet():BelongsTo
     {
