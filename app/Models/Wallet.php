651ad155cc383928452989f91fe18f716b806e0b
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-11-12 09:45:06 +0330

    modify Wallet.php

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -8,16 +8,44 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Wallet extends Model
 {
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'user_id',
         'current_balance',
         'remaining_balance',
     ];
+
+    /**
+     * @param $user
+     * @return mixed
+     */
+    public static function createUserWallet($user)
+    {
+        return Wallet::firstOrCreate(['user_id'=>$user]);
+    }
+
+    /**
+     * @param $user
+     * @return mixed
+     */
+    public static function GetUserWallet($user)
+    {
+        return wallet::where('user_id' , $user)->first();
+    }
+
+    /**
+     * @return BelongsTo
+     */
     public function user():BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * @return HasMany
+     */
     public function walletPayments():HasMany
     {
         return $this->hasMany(WalletPayment::class);
